82d7ca3ce0b56aea6a8a1b30578fada529f4352a
Frankie Deng <frankie.deng@panall.cn>
2021-07-30 21:19:42 +0800

    Modify

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'user'], function () {
+Route::group(['prefix' => 'users'], function () {
     Route::view('/', 'user/index');
 });
 
