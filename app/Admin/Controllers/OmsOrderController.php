7ce6b44f8077554886b2567d47b9ac64b8e87c54
Frankie Deng <frankie.deng@panall.cn>
2021-08-15 11:32:55 +0800

    Modify

--- app/Admin/Controllers/OmsOrderController.php
+++ app/Admin/Controllers/OmsOrderController.php
@@ -3,7 +3,10 @@
 namespace App\Admin\Controllers;
 
 use App\Admin\Repositories\OmsOrder;
-use App\Admin\Repositories\OmsUser;
+use App\Models\OmsProduct;
+use App\Models\OmsProductCategory;
+use App\Models\OmsProductSku;
+use App\Models\OmsUser;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
 use Dcat\Admin\Form\NestedForm;
@@ -57,7 +60,9 @@ class OmsOrderController extends AdminController
     {
         return Form::make(OmsOrder::with("OrderDetail"), function (Form $form) {
             $form->display('order_num');
-            //  $form->text('user_id', '下单用户')->$form->text('name');
+            $form->display('user_id', '下单用户')->customFormat(function () {
+                return OmsUser::find($this->user_id)->mobile;
+            });
             $form->display('mobile');
             $form->display('province');
             $form->display('city');
@@ -69,11 +74,19 @@ class OmsOrderController extends AdminController
                 3 => "已付款",
                 4 => "已发货"
             ]);
-            $form->hasMany('OrderDetail', function (NestedForm $form) {
-                $form->text('sku_id');
-                $form->text('price');
-                $form->text('qty');
-            })->useTable();
+            $form->hasMany('OrderDetail', '订单详情', function (NestedForm $form) {
+                $form->display('sku_id', '品牌')->customFormat(function () {
+                    return OmsProductCategory::find(OmsProduct::find(OmsProductSku::find($this->sku_id)->product_id)->category_id)->name;
+                });
+                $form->display('sku_id', '产品名称')->customFormat(function () {
+                    return OmsProduct::find(OmsProductSku::find($this->sku_id)->product_id)->name;
+                });
+                $form->display('sku_id', '规格')->customFormat(function () {
+                    return OmsProductSku::find($this->sku_id)->sku_name;
+                });
+                $form->display('price');
+                $form->display('qty');
+            })->useTable()->disableCreate();
         });
     }
 }
